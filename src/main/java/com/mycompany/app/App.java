22b96ea37e25fc0c75caa89b70842a0ae796a712
Patrick Mayer <pmyuser69@yahoo.fr>
2018-02-11 22:10:06 +0100

    reset change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,40 +1,21 @@
 package com.mycompany.app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Deque;
-import java.util.Queue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 /**
  * Hello world!
  */
-public class App {
+public class App
+{
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private final String message = "Hello World!";
 
-    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
+    public App() {}
 
-
-    public void greet(String who) {
-        LOGGER.info("Hello {}!", who);
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
     }
 
-    public void run() throws InterruptedException {
-
-        Deque d;
-        Queue q;
-
-        executorService.scheduleAtFixedRate(() -> greet("World"), 2, 2, TimeUnit.SECONDS);
-        executorService.scheduleAtFixedRate(() -> greet("Everybody"), 2, 2, TimeUnit.SECONDS);
-        Thread.sleep(20000);
-        executorService.shutdown();
+    private final String getMessage() {
+        return message;
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        new App().run();
-    }
 }
